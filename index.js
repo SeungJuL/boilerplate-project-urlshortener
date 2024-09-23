2686af7f8f160da88a1354fbd956b1b5fc4c6b2a
SeungJuL <seungju.lee@ufl.edu>
2024-09-23 18:24:40 +0900

    added /api/shorturl get and post apis

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const dns = require('dns');
+const urlParser = require('url');
+
+express.urlencoded();
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -18,6 +22,39 @@ app.get('/', function(req, res) {
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
+
+// middleware function for checking url validation
+function urlCheck(req, res, next) {
+  try { // if unable to parse
+    const url = req.body.url;
+    dns.lookup(urlParser.parse(url).hostname, (err, address, family) => {
+      if (!address) {
+        return res.json({ error: 'invalid url' });
+      }
+      else {
+        next();
+      }
+    });
+  } catch (err) {
+    return res.json({ error: 'invalid url' });
+  }
+
+}
+
+let urlArray = [];
+
+app.post('/api/shorturl', urlCheck, (req, res) => {
+  urlArray.push(req.body.url);
+  res.json({
+    original_url: req.body.url,
+    short_url: urlArray.length - 1
+  })
+})
+
+app.get('/api/shorturl/:id', (req, res) => {
+  res.redirect(urlArray[req.params.id]);
+})
+
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
